68a7d30419bad00bc3e18ae1b3abcfa21643c999
yernurKalt <kaltazik009@gmail.com>
2025-12-27 12:30:34 +0500

    done relationship4

--- app/dao/product_dao.py
+++ app/dao/product_dao.py
@@ -19,10 +19,7 @@ class ProductDAO(BaseDAO):
 
     @classmethod
     async def output_product_addition(cls, product: ProductModel):
-        stmt = select(ProductModel).options(
-        selectinload(ProductModel.category),
-        selectinload(ProductModel.supplier)
-    ).where(ProductModel.id == product.id)
+        stmt = select(ProductModel).where(ProductModel.id == product.id)
         async with async_session_maker() as session:
             result = await session.execute(stmt)
             result = result.scalar_one_or_none()
